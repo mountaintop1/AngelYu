c99ced69f93da08415652e937d7c6ff39314e3ce
Olalekan Adegoke <oadegoke11@gmail.com>
2023-03-05 08:15:46 -0700

    Revert "text file"
    This reverts commit 5e426b0dc2b27c634fbc6950a5e635540b3914c9.

--- text_file.py
+++
@@ -1,8 +0,0 @@
-Undoing commits
-git reset --soft HEAD^ # Removes the last commit, keeps changed staged
-git reset --mixed HEAD^ # Unstages the changes as well
-git reset --hard HEAD^ # Discards local changes
-Reverting commits
-git revert 72856ea # Reverts the given commit
-git revert HEAD~3.. # Reverts the last three commits
-git revert --no-commit HEAD~3..
