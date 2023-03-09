50862e344e53265ee9152a33710e6266267a20e9
Olalekan Adegoke <oadegoke11@gmail.com>
2023-03-09 04:46:58 +0000

    How to use Pandas Library

--- day25/main.py
+++ day25/main.py
@@ -2,7 +2,8 @@
 
 # import csv
 import pandas
-CSV_FILE = r"c:\Users\Olalekan\Desktop\Python\Angela_Yu\day25\weather_data.csv"
+
+CSV_FILE = "./AngelYu/day25/weather_data.csv"
 
 # with open(CSV_FILE) as data_file:
 #     data = data_file.readlines()
@@ -16,5 +17,40 @@ CSV_FILE = r"c:\Users\Olalekan\Desktop\Python\Angela_Yu\day25\weather_data.csv"
 #             temperature.append(int(row[1]))
 #     print(temperature)
 
-data = pandas.read_csv(CSV_FILE)
-print(data["temp"])
+# data = pandas.read_csv(CSV_FILE)
+# data_dic = data.to_dict()
+# condition = data.condition.str.lower()
+# print(condition)
+# print(data_dic)
+# print(data["temp"])
+# print(data["temp"].to_list())
+# print(data["temp"].max())
+# print(data["condition"])
+# print(data["temp"].mean())
+
+# Get data in row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+#monday = data[data.temp == data.temp.max()]
+# condiftion = monday.condition
+# print(condiftion)
+
+# data_dict = {"students": ["Amy", "James", "Angela"],'scores': [76, 56, 65]}
+
+# data1 = pandas.DataFrame(data_dict)
+# print(data1)
+# data1.to_csv("AngelYu/day25/new_data.csv")
+
+central_park_file = "./AngelYu/day25/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv"
+
+# gray, cinnamon, black
+
+data = pandas.read_csv(central_park_file)
+data1 = data["Primary Fur Color"].value_counts()
+data_csv = pandas.DataFrame(data1).to_csv("AngelYu/day25/squirrel_count.csv")
+# data_grey = data[data["Primary Fur Color"] == "Gray"]  # Get data with gray fur
+#test_count = pandas.Series(data_grey)
+# data_count = data_grey.count()  # Get the number of gray squirrels in the data
+# print(data_count)
+
