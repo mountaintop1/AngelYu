a2ac2fb7d9d536d7e6ceab304ddb2524200d1a59
Olalekan Adegoke <oadegoke11@gmail.com>
2023-03-22 03:58:31 +0000

    nato aplhabet completed

--- day26_nato_phonetic_alp/main.py
+++ day26_nato_phonetic_alp/main.py
@@ -9,7 +9,9 @@ for (key, value) in student_dict.items():
     pass
 
 import pandas
-student_data_frame = pandas.DataFrame(nato)
+
+data = pandas.read_csv("./day26_nato_phonetic_alp/nato_phonetic_alphabet.csv")
+student_data_frame = pandas.DataFrame(data)
 
 #Loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
@@ -21,7 +23,11 @@ for (index, row) in student_data_frame.iterrows():
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 #TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+result = {row.letter: row.code for (index, row) in student_data_frame.iterrows()}
+
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
+word = input("Enter a word: ").upper()
+output_list = [result[letter] for letter in word]
+print(output_list)
