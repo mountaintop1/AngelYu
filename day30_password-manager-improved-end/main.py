31cb8d6e53180c1042065af88359bdf36c27b7d0
Olalekan Adegoke <oadegoke11@gmail.com>
2023-05-30 20:29:17 -0600

    test_day30

--- day30_password-manager-improved-end/main.py
+++ day30_password-manager-improved-end/main.py
@@ -5,7 +5,7 @@ import pyperclip
 import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-
+# noted
 #Password Generator Project
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
